d60b72cf31dc690d56423f894534b3e0fe0059d4
Stefan Roets <stefanroets@Stefans-MacBook-Pro.local>
2021-06-01 12:31:10 +0200

    Primitive Types Recap and the String Data Types

--- Section 3/String/src/com/company/Main.java
+++ Section 3/String/src/com/company/Main.java
@@ -15,7 +15,7 @@ public class Main {
         System.out.println("myString is equal to " + myString);
         myString = myString + ", and this is more.";
         System.out.println("myString is equal to " + myString);
-        myString = myString + " \u00A9 2019";
+        myString = myString + " \u00A9 2021";
         System.out.println("myString is equal to " + myString);
         String numberString = "250.55";
         numberString = numberString + "49.95";
