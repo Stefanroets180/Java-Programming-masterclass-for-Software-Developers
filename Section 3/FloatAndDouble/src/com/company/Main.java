b2705f32efc2ea6737e2dcb5ec4024b85f5fae61
Stefan Roets <stefanroets@Stefans-MacBook-Pro.local>
2021-06-01 10:47:52 +0200

    Floating Point Precision and a Challenge

--- Section 3/FloatAndDouble/src/com/company/Main.java
+++ Section 3/FloatAndDouble/src/com/company/Main.java
@@ -14,8 +14,19 @@ public class Main {
         System.out.println("Double minimum value = " + myMinDoubleValue);
         System.out.println("Double maximum value = " + myMaxDoubleValue);
 
-        int myIntValue = 5;
-        float myFloatValue = (float ) 5.25;
-        double myDoubleValue = 5.25;
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5d / 3d;
+        System.out.println("MyIntValue = " + myIntValue);
+        System.out.println("MyFloatValue = " + myFloatValue);
+        System.out.println("MyDouleValue = " + myDoubleValue);
+
+        double numberOfPounds = 200d;
+        double convertedKilograms = numberOfPounds * 0.45359231d;
+        System.out.println("Converted Kilograms = " + convertedKilograms);
+
+        double pi = 3.1415927d;
+        double anotherNumber = 3_000_000.4_567_890d;
+        System.out.println("Convert Kilograms = " + anotherNumber);
     }
 }
