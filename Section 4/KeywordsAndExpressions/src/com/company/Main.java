ac6dfe6d7540cca78e36d916ff1880cd63bbcc9d
Stefan Roets <stefanroets@Stefans-MacBook-Pro.local>
2021-06-02 07:51:17 +0200

    Keywords And Expressions fix

--- Section 4/KeywordsAndExpressions/src/com/company/Main.java
+++ Section 4/KeywordsAndExpressions/src/com/company/Main.java
@@ -3,33 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+        //a mile is equal to 1.609344 kilometres
+        double kilometres = (100 * 1.609344);
+        int highScore = 50;
 
-        calculateScore(gameOver, score, levelCompleted, bonus);
-
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
-
-        calculateScore(gameOver, score, levelCompleted, bonus);
-
-
-    }
-
-    public static int calculateScore(boolean gameOver,int score, int levelCompleted, int bonus) {
-
-        if(gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            finalScore += 2000;
-            System.out.println("Your final score was " + finalScore);
-            return finalScore;
+        if (highScore == 50) {
+            System.out.println("This is an expression");
         }
 
-        return -1;
-
+        //in the following code, that I will type below, write down what part of the code
+        //are expressions.
+        int score = 100;
+        if (score > 99) {
+            System.out.println("You got the high score!");
+            score = 0;
+        }
     }
-
 }
+
