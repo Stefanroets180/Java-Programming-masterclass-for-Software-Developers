58997212cf8f60672d46a0b82dfd52a6bd0f949b
Stefan Roets <stefanroets@Stefans-MacBook-Pro.local>
2021-06-03 17:41:49 +0200

    Speed Converter

---
+++ Section 4/StatementsWhiteSpaceAndIndenting/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVariable = 50;
+        if (myVariable == 50)
+            System.out.println("printed");
+
+        myVariable++;
+        myVariable--;
+        System.out.println("this is test");
+
+        System.out.println("This is " +
+                "another" +
+                " still more.");
+
+        int anotherVariable = 50;
+        myVariable--;
+        System.out.println("this is another one");
+
+
+    }
+}
