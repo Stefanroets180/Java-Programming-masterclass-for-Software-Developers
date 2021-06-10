3c95e15189f80a2eca17f21f42ce4b0d8f3153fb
Stefan Roets <stefanroetscode@gmail.com>
2021-06-10 15:46:28 +0200

    Sum 3 and 5 Challenge

--- Section 5/TheForStatementAndChallengeExercise/src/com/company/Main.java
+++ Section 5/TheForStatementAndChallengeExercise/src/com/company/Main.java
@@ -8,15 +8,15 @@ public class Main {
         System.out.println("10,000 at 4% interest = " + calculateInterest(10000.0, 4.0));
         System.out.println("10,000 at 5% interest = " + calculateInterest(10000.0, 5.0));
 
-        for(int i=0; i<5; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println("Loop " + i + " hello!");
         }
 
         // using the for statement, call the calculateInterest method with
         // the amount of 10000 with an interestRate of 2,3,4,5,6,7, and 8
         // and print the results to the console window.
-        for(int i=2; i<9; i++) {
-            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f",calculateInterest(10000.0, i)));
+        for (int i = 2; i < 9; i++) {
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
         }
 
         System.out.println("****************");
@@ -24,16 +24,16 @@ public class Main {
         // How would you modify the for loop above to do the same thing as
         // Shown but to start from 8% and work back to 2%
 
-        for(int i=8; i>=2; i--) {
-            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f",calculateInterest(10000.0, i)));
+        for (int i = 8; i >= 2; i--) {
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
         }
 
         int count = 0;
-        for(int i=10; i<50; i++) {
-            if(isPrime(i)) {
+        for (int i = 10; i < 50; i++) {
+            if (isPrime(i)) {
                 count++;
                 System.out.println("Number " + i + " is a prime number");
-                if(count == 10) {
+                if (count == 10) {
                     System.out.println("Exiting for loop");
                     break;
                 }
@@ -53,13 +53,13 @@ public class Main {
 
     public static boolean isPrime(int n) {
 
-        if(n == 1) {
+        if (n == 1) {
             return false;
         }
 
-        for(int i=2; i <= n/2; i++) {
-            System.out.println("Looping " +i);
-            if(n % i == 0) {
+        for (int i = 2; i <= n / 2; i++) {
+            System.out.println("Looping " + i);
+            if (n % i == 0) {
                 return false;
             }
         }
@@ -70,6 +70,6 @@ public class Main {
 
 
     public static double calculateInterest(double amount, double interestRate) {
-        return(amount *(interestRate/100));
+        return (amount * (interestRate / 100));
     }
 }
