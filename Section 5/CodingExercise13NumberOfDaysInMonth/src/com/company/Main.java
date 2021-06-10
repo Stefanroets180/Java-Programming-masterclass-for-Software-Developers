0414dc7a203e0ee41dc759879491c82e13702c06
Stefan Roets <stefanroetscode@gmail.com>
2021-06-10 14:55:51 +0200

    Number of Days In Month

--- Section 5/CodingExercise13NumberOfDaysInMonth/src/com/company/Main.java
+++ Section 5/CodingExercise13NumberOfDaysInMonth/src/com/company/Main.java
@@ -3,10 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(NumberOfDaysInMonth.getDaysInMonth(1, 2021));
-        System.out.println(NumberOfDaysInMonth.getDaysInMonth(2, 2021));
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(1, 2020));
         System.out.println(NumberOfDaysInMonth.getDaysInMonth(2, 2020));
-        System.out.println(NumberOfDaysInMonth.getDaysInMonth(-1, 2021));
-        System.out.println(NumberOfDaysInMonth.getDaysInMonth(1, -2021));
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(2, 2018));
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(-1, 2020));
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(1, -2020));
     }
 }
