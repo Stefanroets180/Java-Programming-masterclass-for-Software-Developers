972ac74ed8b12d741b65adb6fc2e301a58465f1b
Stefan Roets <stefanroetscode@gmail.com>
2021-06-10 16:58:49 +0200

    Exercise 14 (Sum odd) to 23 (Number To Words)

---
+++ Section 5/CodingExercise17EvenDigitSum/src/com/company/EvenDigitSum.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class EvenDigitSum {
+
+    public static int getEvenDigitSum(int number) {
+        if (number < 0) {
+            return -1;
+        }
+
+        int digit, sum = 0;
+        while (number > 0) {
+            digit = number % 10;
+            sum += digit % 2 == 0? digit: 0;
+            number /= 10;
+        }
+        return sum;
+    }
+}
