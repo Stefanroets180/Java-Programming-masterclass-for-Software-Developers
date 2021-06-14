d0edad2638b7dd33fc8584484c61b748b5ca5e3c
Stefan Roets <stefanroetscode@gmail.com>
2021-06-14 11:27:24 +0200

    Polymorphism Part 2

--- Section 7/PolymorphismPart2/src/com/company/Main.java
+++ Section 7/PolymorphismPart2/src/com/company/Main.java
@@ -105,7 +105,7 @@ public class Main {
     public static void main(String[] args) {
 	    // We are going to go back to the car analogy.
         // Create a base class called Car
-        // It should have a few fields that would be appropriate for a generice car calss.
+        // It should have a few fields that would be appropriate for a generic car calls.
         // engine, cylinders, wheels, etc.
         // Constructor should initialize cylinders (number of) and name, and set wheels to 4
         // and engine to true. Cylinders and names would be passed parameters.
