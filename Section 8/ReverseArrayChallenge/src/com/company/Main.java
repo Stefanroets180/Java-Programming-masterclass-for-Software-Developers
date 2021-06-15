074176746086758b40a0813a98ce727b0d89b970
Stefan Roets <stefanroetscode@gmail.com>
2021-06-15 16:24:35 +0200

    List and ArrayList

---
+++ Section 8/ReverseArrayChallenge/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] ints = new int[] {1,2,3,4,5,6,7,8};
+        System.out.println("" + Arrays.toString(ints));
+        reverse(ints);
+        System.out.println("" + Arrays.toString(ints));
+    }
+    private static void reverse(int[] array) {
+        /*
+         Only loop half way, then all are swapped.
+         If loop whole array, elements are swapped back again
+        */
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = temp;
+        }
+    }
+}
