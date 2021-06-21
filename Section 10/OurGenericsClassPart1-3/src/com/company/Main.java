45dc4083837d4721ba65a1131ec7bf3b62b0603b
Stefan Roets <stefanroetscode@gmail.com>
2021-06-21 15:24:50 +0200

    Our Generics Class Part 3

--- Section 10/OurGenericsClassPart1-3/src/com/company/Main.java
+++ Section 10/OurGenericsClassPart1-3/src/com/company/Main.java
@@ -1,13 +1,16 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
 
     public static void main(String[] args) {
-	    BaseballPlayer joe = new BaseballPlayer("Joe");
+        FootballPlayer joe = new FootballPlayer("Joe");
         BaseballPlayer pat = new BaseballPlayer("Pat");
         SoccerPlayer beckham = new SoccerPlayer("Beckham");
 
-        Team<BaseballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
+        Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
         adelaideCrows.addPlayer(joe);
 //        adelaideCrows.addPlayer(pat);
 //        adelaideCrows.addPlayer(beckham);
@@ -24,13 +27,27 @@ public class Main {
         FootballPlayer banks = new FootballPlayer("Gordon");
         melbourne.addPlayer(banks);
 
-        Team<BaseballPlayer> hawthorn= new Team<>("Hawthorn");
-        Team<BaseballPlayer> fremantle= new Team<>("Fremantle");
+        Team<FootballPlayer> hawthorn= new Team<>("Hawthorn");
+        Team<FootballPlayer> fremantle= new Team<>("Fremantle");
 
         hawthorn.matchResult(fremantle, 1, 0);
         hawthorn.matchResult(adelaideCrows, 3, 8);
 
         adelaideCrows.matchResult(fremantle, 2, 1);
-        adelaideCrows.matchResult(baseballTeam, 1, 1);
+//        adelaideCrows.matchResult(baseballTeam, 1, 1);
+
+        System.out.println("Rankings");
+        System.out.println(adelaideCrows.getName() + ": " + adelaideCrows.ranking());
+        System.out.println(melbourne.getName() + ": " + melbourne.ranking());
+        System.out.println(fremantle.getName() + ": " + fremantle.ranking());
+        System.out.println(hawthorn.getName() + ": " + hawthorn.ranking());
+
+        System.out.println(adelaideCrows.compareTo(melbourne));
+        System.out.println(adelaideCrows.compareTo(hawthorn));
+        System.out.println(hawthorn.compareTo(adelaideCrows));
+        System.out.println(melbourne.compareTo(fremantle));
+
+        ArrayList<Team> teams = new ArrayList<>();
+        Collections.sort(teams);
     }
 }
--- Section 10/OurGenericsClassPart1-3/src/com/company/Team.java
+++ Section 10/OurGenericsClassPart1-3/src/com/company/Team.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.ArrayList;
 
-public class Team<T extends Player> {
+public class Team<T extends Player> implements Comparable<Team<T>> {
     private String name;
     int played = 0;
     int won = 0;
@@ -61,6 +61,16 @@ public class Team<T extends Player> {
         return (won * 2) + tied;
     }
 
+    @Override
+    public int compareTo(Team<T> team) {
+        if(this.ranking() > team.ranking()) {
+            return -1;
+        } else if(this.ranking() < team.ranking()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
 
 
