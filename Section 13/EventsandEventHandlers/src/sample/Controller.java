62af89faf918a2f310ca1248960c3148cad1a3d0
Stefan Roets <stefanroetscode@gmail.com>
2021-07-04 13:18:43 +0200

    Base Interface Add Change Listener

--- Section 13/EventsandEventHandlers/src/sample/Controller.java
+++ Section 13/EventsandEventHandlers/src/sample/Controller.java
@@ -33,20 +33,20 @@ public class Controller {
         } else if(e.getSource().equals(byeButton)) {
             System.out.println("Bye, " + nameField.getText());
         }
-        
+
         Runnable task = new Runnable() {
             @Override
             public void run() {
                 try {
-                    String s = Platform.isFxApplicationThread() ? "UI Thread" : "Background Thread";
+                    String s = Platform.isFxApplicationThread() ? "UI Thread" : "Background Thread..";
                     System.out.println("I'm going to sleep on the: " + s);
                     Thread.sleep(10000);
                     Platform.runLater(new Runnable() {
                         @Override
                         public void run() {
-                            String s = Platform.isFxApplicationThread() ? "UI Thread" : "Background Thread";
+                            String s = Platform.isFxApplicationThread() ? "UI Thread" : "Background Thread..";
                             System.out.println("I'm updating the label on the: " + s);
-                            ourLabel.setText("We did something!");
+                            ourLabel.setText("We did something haha !!");
                         }
                     });
                 } catch(InterruptedException event) {
