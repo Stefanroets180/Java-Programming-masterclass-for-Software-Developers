27865d72687797bf09949be87f402f80738fc86a
Stefan Roets <stefanroetscode@gmail.com>
2021-07-06 11:03:33 +0200

    Singletons And Load and Save ToDo Items from/to Disk

--- Section 13/TodoList/src/com/stefanroets/todolist/Controller.java
+++ Section 13/TodoList/src/com/stefanroets/todolist/Controller.java
@@ -1,5 +1,6 @@
 package com.stefanroets.todolist;
 
+import com.stefanroets.todolist.datamodel.TodoData;
 import com.stefanroets.todolist.datamodel.TodoItem;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -29,23 +30,25 @@ public class Controller {
     private Label deadlineLabel;
 
     public void initialize() {
-        TodoItem item1 = new TodoItem("Mail birthday card", "Buy a 30th birthday card for John",
-                LocalDate.of(2021, Month.JULY, 25));
-        TodoItem item2 = new TodoItem("Doctor's Appointment", "See Dr. Smith at 123 Main Street.  Bring paperwork",
-                LocalDate.of(2021, Month.AUGUST, 23));
-        TodoItem item3 = new TodoItem("Finish design proposal for client", "I promised Mike I'd email website mockups by Friday 22nd April",
-                LocalDate.of(2021, Month.JULY, 22));
-        TodoItem item4 = new TodoItem("Pickup Doug at the train station", "Doug's arriving on March 23 on the 5:00 train",
-                LocalDate.of(2021, Month.JUNE, 23));
-        TodoItem item5 = new TodoItem("Pick up dry cleaning", "The clothes should be ready by Wednesday",
-                LocalDate.of(2021, Month.JULY,20));
-
-        todoItems = new ArrayList<TodoItem>();
-        todoItems.add(item1);
-        todoItems.add(item2);
-        todoItems.add(item3);
-        todoItems.add(item4);
-        todoItems.add(item5);
+//        TodoItem item1 = new TodoItem("Mail birthday card", "Buy a 30th birthday card for John",
+//                LocalDate.of(2021, Month.JULY, 25));
+//        TodoItem item2 = new TodoItem("Doctor's Appointment", "See Dr. Smith at 123 Main Street.  Bring paperwork",
+//                LocalDate.of(2021, Month.AUGUST, 23));
+//        TodoItem item3 = new TodoItem("Finish design proposal for client", "I promised Mike I'd email website mockups by Friday 22nd April",
+//                LocalDate.of(2021, Month.JULY, 22));
+//        TodoItem item4 = new TodoItem("Pickup Doug at the train station", "Doug's arriving on March 23 on the 5:00 train",
+//                LocalDate.of(2021, Month.JUNE, 23));
+//        TodoItem item5 = new TodoItem("Pick up dry cleaning", "The clothes should be ready by Wednesday",
+//                LocalDate.of(2021, Month.JULY,20));
+//
+//        todoItems = new ArrayList<TodoItem>();
+//        todoItems.add(item1);
+//        todoItems.add(item2);
+//        todoItems.add(item3);
+//        todoItems.add(item4);
+//        todoItems.add(item5);
+//
+//        TodoData.getInstance().setTodoItems(todoItems);
 
         todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
             @Override
@@ -53,13 +56,13 @@ public class Controller {
                 if(newValue != null) {
                     TodoItem item = todoListView.getSelectionModel().getSelectedItem();
                     itemDetailsTextArea.setText(item.getDetails());
-                    DateTimeFormatter df = DateTimeFormatter.ofPattern("EEEE - d MMMM - yyyy"); // "d M yy");
+                    DateTimeFormatter df = DateTimeFormatter.ofPattern("MMM d, yyyy"); // "d M yy");
                     deadlineLabel.setText(df.format(item.getDeadline()));
                 }
             }
         });
 
-        todoListView.getItems().setAll(todoItems);
+        todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         todoListView.getSelectionModel().selectFirst();
     }
--- Section 13/TodoList/src/com/stefanroets/todolist/Main.java
+++ Section 13/TodoList/src/com/stefanroets/todolist/Main.java
@@ -1,11 +1,13 @@
 package com.stefanroets.todolist;
 
+import com.stefanroets.todolist.datamodel.TodoData;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public class Main extends Application {
@@ -20,6 +22,27 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+
         launch(args);
+    }
+
+    @Override
+    public void stop() throws Exception {
+        try{
+            TodoData.getInstance().storeTodoItems();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void init() throws Exception {
+        try{
+            TodoData.getInstance().loadTodoItems();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
