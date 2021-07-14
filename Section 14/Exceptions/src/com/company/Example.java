b99c0777a08a6e99873e535686849baddf005bde
Stefan Roets <stefanroetscode@gmail.com>
2021-07-14 10:44:53 +0200

    Stack Trace and Call Stack & Catching and throwing Exceptions

--- Section 14/Exceptions/src/com/company/Example.java
+++ Section 14/Exceptions/src/com/company/Example.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Example {
@@ -10,15 +12,32 @@ public class Example {
     }
 
     private static int divide() {
-        int x = getInt();
-        int y = getInt();
+        int x, y;
+        try {
+            x = getInt();
+            y = getInt();
+        } catch(NoSuchElementException e) {
+            throw new ArithmeticException("no suitable input");
+        }
         System.out.println("x is " + x + ", y is " + y);
-        return x / y;
+        try {
+            return x / y;
+        } catch(ArithmeticException e) {
+            throw new ArithmeticException("attempt to divide by zero");
+        }
     }
 
     private static int getInt() {
         Scanner s = new Scanner(System.in);
         System.out.println("Please enter an integer ");
-        return s.nextInt();
+        while(true) {
+            try {
+                return s.nextInt();
+            } catch(InputMismatchException e) {
+                // go round again.  Read past the end of line in the input first
+                s.nextLine();
+                System.out.println("Please enter a number using only the digits 0 to 9 ");
+            }
+        }
     }
 }
